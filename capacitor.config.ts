c020b288130d1938798313773f1750024ae2b547
Abhishekanjana <abhishekanjana85@gmail.com>
2024-06-26 15:07:26 +0530

    working

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.khandelwal.jewels',
   appName: 'khandelwal-jewels',
   webDir: 'dist'
 };
