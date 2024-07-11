b724feeed175526aaaba65ba9afd44a3a4ba55bd
Developerodin <developer@theodin.in>
2024-07-11 15:01:52 +0530

    working

--- src/components/LoginModel.jsx
+++ src/components/LoginModel.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { IonContent, IonModal, IonButton } from '@ionic/react';
+import { IonContent, IonModal, IonButtons,IonBackButton } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 import './LoginModel.css';
 import ContactUsButton from './ContactUsButton';
+
+import { arrowBackSharp } from 'ionicons/icons';
 
 const LoginModel = ({ isOpen, onClose }) => {
   const history = useHistory();
@@ -15,9 +17,21 @@ const LoginModel = ({ isOpen, onClose }) => {
   return (
     <IonModal isOpen={isOpen} onDidDismiss={onClose}>
       <IonContent fullscreen style={{ '--ion-background-color': '#F8EBD8' }}>
+      
         <div className="login-header">
+        <IonButtons slot="">
+          <IonBackButton
+            defaultHref="/"
+            text=""
+            className="back-button"
+            icon={arrowBackSharp}
+            style={{paddingLeft:'20px',color:'white',fontSize:'20px'}}
+          />
+          
+        </IonButtons>
           <img src="assets/Frame 1.png" alt="Logo" className="logo" />
         </div>
+        
         <h2 className='welcome' style={{fontSize:'28px',color:'#a52a2a'}}>Welcome</h2>
         <div className="login-form login">
           <ContactUsButton onClick={handleLoginRedirect} buttonName='Go to Login' />
